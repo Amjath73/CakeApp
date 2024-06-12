43152ab56da2ffc0d9b429438f5f5c904d45e575
Amjath73 <amjathsanker4@gmail.com>
2024-06-13 02:56:58 +0530

    updated

--- app.js
+++ app.js
@@ -18,7 +18,17 @@ app.post("/add", (req, res) => {
     res.json({ "status": "success" })
 })
 
-
+app.get("/view", (req, res) => {
+    cakemodel.find().then(
+        (data) => {
+            res.json(data)
+    }
+    ).catch(
+        (error) => {
+            res.json(error)
+        }
+    )
+})
 
 app.listen(8081, ()=>{
     console.log("server started")
